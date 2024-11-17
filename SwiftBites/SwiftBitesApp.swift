3f8defaf836d54ec13fc7743f4f1ce4f0735ea60
Pankaj Singh <theiterator@gmail.com>
2024-11-17 19:02:27 +0530

    Initial Commit

---
+++ SwiftBites/SwiftBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBitesApp.swift
+//  SwiftBites
+//
+//  Created by Singh, Pankaj on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
