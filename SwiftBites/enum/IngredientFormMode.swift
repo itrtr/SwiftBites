c85b8c71204e9040ca4ad86a3ea8f5502f503b76
Pankaj Singh <theiterator@gmail.com>
2024-11-25 18:09:12 +0530

    Add missing IngredientFormMode file

---
+++ SwiftBites/enum/IngredientFormMode.swift
@@ -0,0 +1,13 @@
+//
+//  IngredientFormMode.swift
+//  SwiftBites
+//
+//  Created by Singh, Pankaj on 22/11/24.
+//
+
+import Foundation
+
+enum IngredientFormMode {
+    case add
+    case edit(Ingredient)
+}
